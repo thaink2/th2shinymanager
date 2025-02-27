d87f3da4e5e339b9110b76974b50f41a352b7d4b
Farid Azouaou <farid.azouaou@thaink2.com>
2025-02-27 12:42:47 +0100

    it weems to work apparently

---
+++ my_reports/belga_devis_app/app.R
@@ -0,0 +1,3 @@
+Sys.setenv(TZ = "Europe/Paris")
+.libPaths('/home/packages/R/')
+th2belgatrans::belgatrans_app_launcher(target_port = 8912)
--- run_app_with_cookies.R
+++ run_app_with_cookies.R
@@ -8,4 +8,4 @@ list()%>%
   jsonlite::write_json("data.json")
 options(shiny.http.response.filter = post_handler)
 ui_with_cookies <- cookies::add_cookie_handlers(dive2ml_ui_auth)
-shinyApp(ui_with_cookies, protected_server, options = list("launch.browser" = T, port = 3838))
+shinyApp(ui_with_cookies, dive2ml_server_auth, options = list("launch.browser" = T, port = 3838))
